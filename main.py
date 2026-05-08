688b3afe85c96a596b86650f2df5b9a7808d3b13
Carl Leyco <132826426+carlleyco@users.noreply.github.com>
2026-05-08 22:47:12 +0800

    added Animation of Satellite with a Trail (Circular Orbit)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 # Constants
 G = 6.674e-11   # Gravitational Constant
@@ -7,8 +8,7 @@ M = 5.972e24    # Earth Mass
 R = 6.371e6     # Earth Radius
 
 def start(vmultiplier, steps=10000, dt=10.0):
-    altitude = 400e3
-    r0 = R + altitude
+    r0 = R + 400e3
     vcircular = np.sqrt(G * M / r0) # circular velocity
 
     x,y = r0, 0.0
@@ -36,27 +36,47 @@ def start(vmultiplier, steps=10000, dt=10.0):
 
     return np.array(xlist), np.array(ylist)
 
-types = [
-    (1.0,'Circular','orange'),
-    (1.2,'Elliptical','purple'),
-    (np.sqrt(2),'Escape','green'),
-]
+xarray, yarray = start(1.0)
 
-fig,ax = plt.subplots(figsize=(9,9))
-earth = plt.Circle((0,0), R, color='royalblue', alpha = 0.8, label='Earth')
+fig,ax=plt.subplots(figsize=(8,8))
+
+earth = plt.Circle((0,0), R, color='royalblue', alpha=0.8)
 ax.add_patch(earth)
 
-for vmultiplier, label, color in types:
-    xarray, yarray = start(vmultiplier)
-    ax.plot(xarray,yarray, color=color, lw=1.5, label=f'{label} (v x {vmultiplier:.3f})')
-    ax.plot(xarray[0], yarray[0], 'o', color=color, markersize=6)
+orbit = ax.plot([], [], color='orange', lw=1.2, alpha=0.5)[0]
+satellite = ax.plot([], [], 'yo', markersize=8, label='Satellite')[0]
+trail = ax.plot([], [], color='orange', lw=1.5)[0]
 
+limit = 1.5 * (R + 400e3)
+ax.set_xlim(-limit, limit)
+ax.set_ylim(-limit, limit)
 ax.set_aspect('equal')
 ax.set_xlabel('x (m)')
 ax.set_ylabel('y (m)')
-ax.set_title('Orbital Mechanics [Circular, Elliptical, Escape]')
-ax.legend(loc='upper right')
+ax.set_title('Orbital Mechanics [Satellite]')
+ax.legend()
 ax.grid(True, alpha=0.3)
-plt.tight_layout()
 
+ax.plot(xarray,yarray, color='orange', lw=0.8, alpha=0.3)
+
+def init():
+    orbit.set_data([], [])
+    satellite.set_data([], [])
+    return orbit, satellite
+
+def update(frame):
+    start = max(0, frame - 100)
+    trail.set_data(xarray[start:frame], yarray[start:frame])
+    satellite.set_data([xarray[frame]], [yarray[frame]])
+    return trail, satellite
+
+step = 3
+frames = range(0, len(xarray), step)
+
+ani = animation.FuncAnimation(
+    fig,update, frames=frames,
+    init_func=init, interval=20, blit=True
+)
+
+plt.tight_layout()
 plt.show()
